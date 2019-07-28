b74c57d6a0aa26618432a5830a94702a3609d9b6
jinjin99 <1656056936@qq.com>
2019-07-28 10:15:08 +0800

    点击搜索切换到搜索页面，切换关注，推荐页面

--- script/home/home.js
+++ script/home/home.js
@@ -1,3 +1,4 @@
+//点赞
 $(".like").click(function(){
 	let oLikeNum = $(this).find('.like-num');
 	let likeNum = oLikeNum.text();
@@ -9,3 +10,35 @@ $(".like").click(function(){
 		oLikeNum.text(--likeNum);
 	}
 });
+//切换关注，推荐栏
+function change(){
+	$('.rec').click(function(){
+		let index = $(this).index();
+		let oSpan = $(this).parent().find('span');
+		oSpan.css('opacity','0');
+		oSpan.eq(index).css('opacity','1');
+		let oContent = $('.main-content');
+		oContent.css('display','none');
+		oContent.eq(index).css('display','block');
+	})
+}
+change();
+
+//点击搜索 跳到搜索页面
+function turnToSearch(){
+	$('.search').click(function(){
+	$('.home').css('display','none');
+	$('.search-web').css('display','block')
+	
+})
+}
+turnToSearch()
+//点击返回，回到首页
+function turnToHome(){
+	$('.return').click(function(){
+	$('.search-web').css('display','none');
+	$('.home').css('display','block');
+})
+
+}
+turnToHome()
