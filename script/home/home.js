8520fd251ee47545b2185c90c54384a544ea7726
jinjin99 <1656056936@qq.com>
2019-07-28 11:03:16 +0800

    切换关注推荐和搜索

--- script/home/home.js
+++ script/home/home.js
@@ -9,3 +9,34 @@ $(".like").click(function(){
 		oLikeNum.text(--likeNum);
 	}
 });
+
+//切换关注推荐页面
+function change(){
+	$('.rec').click(function(){
+		let index = $(this).index();
+		let oSpan = $(this).parent().find('span');
+		oSpan.css('opacity','0');
+		oSpan.eq(index).css('opacity','1');
+		let oContent = $('.main-content');
+		oContent.css('display','none');
+		oContent.eq(index).css('display','block');
+	})
+}
+change();
+
+//点击搜索切换到搜索页面
+function turnToSearch(){
+	$('.search').click(function(){
+		$('.home').css('display','none');
+		$('.search-web').css('display','block');
+	})
+}
+turnToSearch();
+//点击返回切换到主页
+function turnToHome(){
+	$('.return').click(function(){
+		$('.search-web').css('display','none');
+		$('.home').css('display','block');
+	})
+}
+turnToHome();
